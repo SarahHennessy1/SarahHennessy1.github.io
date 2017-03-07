fc2f16e2e06096779c4e9f409285f45086005539
Sarah Hennessy <HennS101@newschool.edu>
2017-03-07 14:19:47 -0500

    Fixed Main Page

---
+++ screensaver/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+var height = Math.floor(Math.random() * 100);
+
+console.log(height);
+
+function setHeight() {
+	$(".flame").css({"height"height+"%"
+});
+
+$("flame").css("height");
+
+});
---
+++ vars_cond_loops/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+	var popCount = 0;
+	var num = 5;
+
+	for (var i=0; i<num; i++) {
+	$("#balloon-container").append("<div class='balloon'></div>");
+	if (i % 2 === 0) {
+	$(".balloon").last().addClass("big");
+	 }
+	};
+
+	//for (i<num;) {
+	//$("#balloon-container").addClass("win");
+	// }
+	//};
+
+	$(".balloon").on("mouseenter", function() {
+	$(this).addClass("pop");
+	popCount = popCount + 1;
+	$("#pop-target").html(popCount);
+
+	if (popCount===num) {
+	$("body").addClass("win");	
+	$("body").html("GAME OVER");
+	 } 
+	});
+
+
+});
