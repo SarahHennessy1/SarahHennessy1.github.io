c8823f27b7ea5e939bb7f29b3ceba9bf5441b7c2
Sarah Hennessy <HennS101@newschool.edu>
2017-03-07 21:34:53 -0500

    Continued Randomization

--- screensaver/script.js
+++ screensaver/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 
-var firstHeight = Math.floor(Math.random() * 200 + 10);
-var secondHeight = Math.floor(Math.random() * 200 + 20);
-var thirdHeight = Math.floor(Math.random() * 200 + 30);
-var fourthHeight = Math.floor(Math.random() * 200 + 40);
-var fifthHeight = Math.floor(Math.random() * 200 + 40);
-var sixthHeight = Math.floor(Math.random() * 200 + 30);
-var seventhHeight = Math.floor(Math.random() * 200 + 20);
-var eigthHeight = Math.floor(Math.random() * 200 + 10);
+function changeHeight() {
+	var firstHeight = Math.floor(Math.random() * 200 + 100);
+	var secondHeight = Math.floor(Math.random() * 200 + 20);
+	var thirdHeight = Math.floor(Math.random() * 200 + 30);
+	var fourthHeight = Math.floor(Math.random() * 200 + 40);
+	var fifthHeight = Math.floor(Math.random() * 200 + 40);
+	var sixthHeight = Math.floor(Math.random() * 200 + 30);
+	var seventhHeight = Math.floor(Math.random() * 200 + 20);
+	var eigthHeight = Math.floor(Math.random() * 200 + 10);
 
 console.log(firstHeight);
 console.log(secondHeight);
@@ -18,52 +19,32 @@ console.log(sixthHeight);
 console.log(seventhHeight);
 console.log(eigthHeight);
 
-$(document).ready(function () {
-$(".flame").css({height:firstHeight});
-});
+	$(".flame1").css({height:firstHeight});
 
-$(document).ready(function () {
-$(".flame2").css({height:secondHeight});
-});
+	$(".flame2").css({height:secondHeight});
 
-$(document).ready(function () {
-$(".flame3").css({height:thirdHeight});
-});
+	$(".flame3").css({height:thirdHeight});
 
-$(document).ready(function () {
-$(".flame4").css({height:fourthHeight});
-});
+	$(".flame4").css({height:fourthHeight});
 
-$(document).ready(function () {
-$(".flame5").css({height:fifthHeight});
-});
+	$(".flame5").css({height:fifthHeight});
 
-$(document).ready(function () {
-$(".flame6").css({height:sixthHeight});
-});
+	$(".flame6").css({height:sixthHeight});
 
-$(document).ready(function () {
-$(".flame7").css({height:seventhHeight});
-});
+	$(".flame7").css({height:seventhHeight});
 
-$(document).ready(function () {
-$(".flame8").css({height:eigthHeight});
-});
+	$(".flame8").css({height:eigthHeight});
+};
 
-$("div").hover(function () {
-	$(this).toggleClass("max")
-});
 
+window.setInterval(changeHeight, 1000); 
 
 });
 
 
-// function setHeight() {
-//	$(".flame").css({"height"height+"%"
-//});
 
-//$("flame").css("height");
-//$(".flame").onload(function() {
-//var newHeight = $(".flame").val();
-//$(".flame").height(newHeight);
-//});
+
+
+
+
+
